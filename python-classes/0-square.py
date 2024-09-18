379ba9e2bc93672b0f63acd8bbdc7fe3d50998b0
CoeurdArcade <coeurdarcade@gmail.com>
2024-09-18 11:31:34 +0200

    updated script

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,39 +1,8 @@
 #!/usr/bin/python3
-"""Defines a Square class."""
+
+"""Define a class Square."""
+
 
 class Square:
     """Represent a square."""
-
-    def __init__(self, size=0):
-        """Initialize a new Square.
-
-        Args:
-            size (int): The size of the new square.
-        """
-        self.size = size
-
-    @property
-    def size(self):
-        """Get/set the size of the square."""
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
-
-    def area(self):
-        """Return the area of the square."""
-        return self.__size ** 2
-
-    def perimeter(self):
-        """Return the perimeter of the square."""
-        return 4 * self.__size
-
-    def __str__(self):
-        """Return the string representation of the square."""
-        return f"Square with size {self.__size}"
-
+    pass
