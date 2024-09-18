bcf3f8bf9abe964587a5b48a3fe87bc39a6b6151
CoeurdArcade <coeurdarcade@gmail.com>
2024-09-18 11:41:24 +0200

    updated script

--- python-classes/mains/100-main.py
+++ python-classes/mains/100-main.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-SinglyLinkedList = __import__('100-singly_linked_list').SinglyLinkedList
 
+# Import the SinglyLinkedList class from the module
+from singly_linked_list import SinglyLinkedList
+
+# Create an instance of SinglyLinkedList
 sll = SinglyLinkedList()
-sll.sorted_insert(2)
-sll.sorted_insert(5)
-sll.sorted_insert(3)
-sll.sorted_insert(10)
-sll.sorted_insert(1)
-sll.sorted_insert(-4)
-sll.sorted_insert(-3)
-sll.sorted_insert(4)
-sll.sorted_insert(5)
-sll.sorted_insert(12)
-sll.sorted_insert(3)
+
+# List of values to be inserted in the sorted order
+values_to_insert = [2, 5, 3, 10, 1, -4, -3, 4, 5, 12, 3]
+
+# Insert each value into the sorted linked list
+for value in values_to_insert:
+    sll.sorted_insert(value)
+
+# Print the sorted linked list
 print(sll)
 
