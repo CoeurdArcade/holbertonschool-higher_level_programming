e556aad4fa3b6d39b4d42be4105cbee29ecd2459
CoeurdArcade <coeurdarcade@gmail.com>
2024-09-18 11:43:35 +0200

    improved script

--- python-classes/mains/101-main.py
+++ python-classes/mains/101-main.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
+
+# Import the Square class from the '101-square' module
 Square = __import__('101-square').Square
 
-my_square = Square(5, (0, 0))
-print(my_square)
+# Create a Square object with size 5 and position (0, 0)
+square1 = Square(5, (0, 0))
+print(square1)
 
 print("--")
 
-my_square = Square(5, (4, 1))
-print(my_square)
+# Create another Square object with size 5 and position (4, 1)
+square2 = Square(5, (4, 1))
+print(square2)
 
