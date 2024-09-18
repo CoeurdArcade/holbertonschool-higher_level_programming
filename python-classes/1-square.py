6019114ec22b97bcee0451f8d85546a371c92fc5
CoeurdArcade <coeurdarcade@gmail.com>
2024-09-18 11:32:35 +0200

    updated script

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,27 +1,15 @@
 #!/usr/bin/python3
-"""Defines a Square class."""
+
+"""Define a class Square."""
+
 
 class Square:
     """Represent a square."""
 
-    def __init__(self, size=0):
+    def __init__(self, size):
         """Initialize a new Square.
 
         Args:
             size (int): The size of the new square.
         """
-        self.size = size
-
-    @property
-    def size(self):
-        """Get/set the size of the square."""
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
-
+        self.__size = size
