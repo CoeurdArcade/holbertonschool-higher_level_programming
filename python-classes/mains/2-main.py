385aec4c3ae22d1283ca979f9658ab7295d8beda
CoeurdArcade <coeurdarcade@gmail.com>
2024-09-18 10:54:06 +0200

    main optimised

--- python-classes/mains/2-main.py
+++ python-classes/mains/2-main.py
@@ -1,35 +1,31 @@
 #!/usr/bin/python3
+
 Square = __import__('2-square').Square
 
-my_square_1 = Square(3)
-print(type(my_square_1))
-print(my_square_1.__dict__)
+def print_square_info(square):
+    print(type(square))
+    print(square.__dict__)
 
-my_square_2 = Square()
-print(type(my_square_2))
-print(my_square_2.__dict__)
+def main():
+    try:
+        my_square_1 = Square(3)
+        print_square_info(my_square_1)
 
-try:
-    print(my_square_1.size)
-except Exception as e:
-    print(e)
+        my_square_2 = Square()
+        print_square_info(my_square_2)
 
-try:
-    print(my_square_1.__size)
-except Exception as e:
-    print(e)
+        print(my_square_1.size)
+        print(my_square_1.__size)
 
-try:
-    my_square_3 = Square("3")
-    print(type(my_square_3))
-    print(my_square_3.__dict__)
-except Exception as e:
-    print(e)
+        my_square_3 = Square("3")
+        print_square_info(my_square_3)
 
-try:
-    my_square_4 = Square(-89)
-    print(type(my_square_4))
-    print(my_square_4.__dict__)
-except Exception as e:
-    print(e)
+        my_square_4 = Square(-89)
+        print_square_info(my_square_4)
+
+    except Exception as e:
+        print(e)
+
+if __name__ == "__main__":
+    main()
 
