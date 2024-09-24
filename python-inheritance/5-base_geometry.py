ea3277e4faf7e3f5db3d8829737a0f4055ad27c6
CoeurdArcade <coeurdarcade@gmail.com>
2024-09-24 16:59:59 +0200

    script of an empty class BaseGeometry

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Defines an empty class BaseGeometry.
+
+This module contains the definition of the BaseGeometry class, which is intended to serve as a base class for geometric shapes.
+"""
+
+class BaseGeometry:
+    """
+    Represent base geometry.
+
+    This class is currently empty and serves as a placeholder for future geometric functionalities.
+    """
+    pass
+
