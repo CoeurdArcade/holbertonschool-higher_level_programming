29cd4b66409d573cf858b72f5cf522cc13c36c35
CoeurdArcade <coeurdarcade@gmail.com>
2024-09-24 17:01:33 +0200

    script for a class BaseGeometry (based on 5-base_geometry.py)

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Defines a base geometry class BaseGeometry.
+"""
+
+class BaseGeometry:
+    """
+    Represent base geometry.
+    """
+
+    def area(self):
+        """
+        Calculate the area of the geometry.
+
+        Raises:
+            Exception: This method is not implemented.
+        """
+        raise Exception("area() is not implemented")
+
