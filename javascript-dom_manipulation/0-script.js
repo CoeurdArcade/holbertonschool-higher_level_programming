fa659149e531346207a4355e77962081369fcf01
CoeurdArcade <coeurdarcade@gmail.com>
2024-11-28 11:01:38 +0100

    updated script

--- javascript-dom_manipulation/0-script.js
+++ javascript-dom_manipulation/0-script.js
@@ -1,3 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-  document.querySelector('header').style.color = '#FF0000';
-});
+const headerElement = document.querySelector('header');
+
+headerElement.style.color = '#FF0000'
